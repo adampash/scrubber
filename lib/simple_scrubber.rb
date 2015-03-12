9e0f7d2c2d2ec0f4016b5f5d765c3ba537896bda
Adam Pash <adam.pash@gmail.com>
2015-03-12 10:43:08 -0400

    Now scrubbing less data

--- lib/simple_scrubber.rb
+++ lib/simple_scrubber.rb
@@ -3,13 +3,15 @@ require "simple_scrubber/version"
 module SimpleScrubber
   def self.scrub(text, types=[])
     if types.include? :email
-      email_re = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
-      text.gsub!(email_re, "xxx@xxx.xxx")
+      email_re = /\b([A-Z0-9._%+-]+)@([A-Z0-9.-]+\.[A-Z]{2,4})\b/i
+      text.gsub!(email_re, 'xxx@\2')
     end
 
     if types.include? :phone
       phone_re = /\b(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?\b/
-      text.gsub!(phone_re, "XXX-XXX-XXXX")
+      match = text.match phone_re
+      area_code = match[1] || match[2]
+      text.gsub!(phone_re, "#{area_code}-xxx-xxxx")
     end
 
     text
--- spec/lib/simple_scrubber_spec.rb
+++ spec/lib/simple_scrubber_spec.rb
@@ -3,19 +3,22 @@ require_relative '../../lib/simple_scrubber'
 describe SimpleScrubber do
   it "scrubs emails" do
     text = SimpleScrubber.scrub("foo bar adam.pash@gmail.com baz bat", [:email])
-    expect(text).to eq "foo bar xxx@xxx.xxx baz bat"
+    expect(text).to eq "foo bar xxx@gmail.com baz bat"
 
     text = SimpleScrubber.scrub("adam.pash+foo@gmail.com.", [:email])
-    expect(text).to eq "xxx@xxx.xxx."
+    expect(text).to eq "xxx@gmail.com."
   end
 
   it "scrubs phone numbers" do
     text = SimpleScrubber.scrub("foo bar 1(555)555-1234 baz bat", [:phone])
-    expect(text).to eq "foo bar XXX-XXX-XXXX baz bat"
+    expect(text).to eq "foo bar 555-xxx-xxxx baz bat"
+
+    text = SimpleScrubber.scrub("foo bar 1-212-555-1234 baz bat", [:phone])
+    expect(text).to eq "foo bar 212-xxx-xxxx baz bat"
   end
 
   it "scrubs email and phone numbers" do
     text = SimpleScrubber.scrub("contact bar at 1(555)555-1234 or pash@gawker.com", [:email, :phone])
-    expect(text).to eq "contact bar at XXX-XXX-XXXX or xxx@xxx.xxx"
+    expect(text).to eq "contact bar at 555-xxx-xxxx or xxx@gawker.com"
   end
 end
